a347e00dee4a96d7fb5409a8f9b2fa8560474cac
gitgucdomkupiii <124463234+gitgucdomkupiii@users.noreply.github.com>
2023-03-21 15:36:47 +0100

    Add files via upload

---
+++ wycieczki.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wycieczki i urlopy</title>
+    <link rel="stylesheet" type="text" href="styl3.css">
+</head>
+<body>
+    <header>
+        <h1>BIURO PODRÓŻY</h1>
+    </header>
+    <section class="lewy">
+        <h2>KONTAKT</h2>
+        <a href="biuro@wycieczki.pl">Napisz do nas</a>
+        <br>
+        <p>telefon: 555666777</p>
+    </section>
+    <section class="środkowy">
+        <h2>GALERIA</h2>
+    </section>
+    <section class="prawy">
+        <h2>PROMOCJE</h2>
+    </section>
+    <main>
+
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
